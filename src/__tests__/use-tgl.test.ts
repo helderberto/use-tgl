6999601f8c3b730e6a1dd9aad117dfa87ccddf25
Helder Burato Berto <helder.burato@gmail.com>
2021-08-25 12:42:50 +0100

    feat: add state and handlers with tests

--- src/__tests__/use-tgl.test.ts
+++ src/__tests__/use-tgl.test.ts
@@ -1,7 +1,45 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+
 import { useTgl } from '../use-tgl';
 
 describe('useTgl', () => {
   it('shallow validate if useTgl is defined', () => {
     expect(useTgl).toBeDefined();
+  });
+
+  it('should return current state as true', () => {
+    const { result } = renderHook(() => useTgl());
+
+    expect(result.current.state).toBeTruthy();
+  });
+
+  it('should return current state as false', () => {
+    const { result } = renderHook(() => useTgl(false));
+
+    expect(result.current.state).toBeFalsy();
+  });
+
+  it('should update state when trigger switch()', () => {
+    const { result } = renderHook(() => useTgl());
+
+    act(() => result.current.switch());
+
+    expect(result.current.state).toBeFalsy();
+  });
+
+  it('should turn on with on()', () => {
+    const { result } = renderHook(() => useTgl(false));
+
+    act(() => result.current.on());
+
+    expect(result.current.state).toBeTruthy();
+  });
+
+  it('should turn off with off()', () => {
+    const { result } = renderHook(() => useTgl());
+
+    act(() => result.current.off());
+
+    expect(result.current.state).toBeFalsy();
   });
 });
--- src/use-tgl.ts
+++ src/use-tgl.ts
@@ -1,3 +1,25 @@
-export function useTgl(): void {
-  return;
+import { useState, useMemo } from 'react';
+
+interface UseTgl {
+  state: boolean;
+  on(): void;
+  off(): void;
+  switch(): void;
+}
+
+type initialState = boolean;
+
+export function useTgl(initial: initialState = true): UseTgl {
+  const [state, setState] = useState(initial);
+
+  const handlers = useMemo(
+    () => ({
+      on: () => setState(true),
+      off: () => setState(false),
+      switch: () => setState(!state),
+    }),
+    [],
+  );
+
+  return { state, ...handlers };
 }
